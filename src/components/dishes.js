a1e32f85acfa2be6c94dc2ae65ced4b50ce10dc6
Drasenberg <macoel14@gmail.com>
2021-11-06 00:11:25 +0100

    Update dishes.js

--- src/components/dishes.js
+++ src/components/dishes.js
@@ -1,9 +1,21 @@
 const Dishes = () => {
   return (
     <div>
-
+      <form>
+        <label for="name-of-dish">Name</label>
+        <input type="text" name="name-of-dish" />
+        <lable for="preparation-time">Preparation Time</lable>
+        <input type="time" name="preparation-time" step="2" />
+        <p>Choose type of dish</p>
+        <label for="pizza">Pizza</label>
+        <input type="radio" name="type-of-dish" id="pizza" />
+        <label for="soup">Soup</label>
+        <input type="radio" name="type-of-dish" id="soup" />
+        <label for="sandwich">Sandwich</label>
+        <input type="radio" name="type-of-dish" id="sandwich" />
+      </form>
     </div>
   );
-}
+};
 
 export default Dishes;
